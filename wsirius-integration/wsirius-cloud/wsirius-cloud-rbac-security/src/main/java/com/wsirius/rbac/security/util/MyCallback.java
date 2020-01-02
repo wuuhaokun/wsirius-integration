bb03d9308f1c390b7c1b62035cacff9a03fcb47d
chao-kun.wu <w.sirius@gamil.com>
2020-01-02 14:10:35 +0800

    Callback 更改帳號版本密碼

---
+++ wsirius-integration/wsirius-cloud/wsirius-cloud-rbac-security/src/main/java/com/wsirius/rbac/security/util/MyCallback.java
@@ -0,0 +1,21 @@
+package com.wsirius.rbac.security.util;
+
+
+/**
+ * <p>
+ * JWT 工具类
+ * </p>
+ *
+ * @package: com.xkcoding.rbac.security.util
+ * @description: JWT 工具类
+ * @author: yangkai.shen
+ * @date: Created in 2018-12-07 13:42
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+public interface MyCallback {
+
+    public void callback(Boolean success);
+
+}
